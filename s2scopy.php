97a34051abc1e72a7e3aefab098885fc6bb9c571
sazzadh88 <sazzadh88@gmail.com>
2016-09-23 22:31:25 +0530

    Create s2scopy.php

---
+++ s2scopy.php
@@ -0,0 +1,19 @@
+<?php
+
+$remote_file_url = 'http://new-server/file.zip';
+ 
+/* New file name and path for this file */
+$local_file = 'files.zip';
+ 
+/* Copy the file from source url to server */
+$copy = copy( $remote_file_url, $local_file );
+ 
+/* Add notice for success/failure */
+if( !$copy ) {
+    echo "Doh! failed to copy $file...\n";
+}
+else{
+    echo "WOOT! success to copy $file...\n";
+}
+
+?>
