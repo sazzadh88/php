211040453ebdcd23f11706338122c9f3fe90a567
Sazzad Hussain <sazzadh88@gmail.com>
2019-06-25 11:55:50 +0530

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,6 +1,8 @@
 File Read
 ---------
 
+:+1: This PR looks great - it's ready to merge! :shipit:
+
 <?php
 $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("webdictionary.txt"));
